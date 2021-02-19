2ef76a26518add9b12cb35b94cf767bad8d3d324
Florian Ledermann <florian.ledermann@tuwien.ac.at>
2021-02-19 12:28:48 +0100

    extend dev scripts

--- _dev.bat
+++ _dev.bat
@@ -1,5 +1,6 @@
-start powershell -noexit -command "npm run serve-dev"
+start powershell -noexit -command "npm run watch-server"
 start powershell -noexit -command "npm run watch-client"
+start powershell -noexit -command "npm run watch-experiment"
 start powershell -noexit
 
 
