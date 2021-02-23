7e39f414dd13350a431c0cb01be63764dfbc8d75
Florian Ledermann <florian.ledermann@tuwien.ac.at>
2021-02-23 18:08:25 +0100

    open smaller dev windows

--- _dev.bat
+++ _dev.bat
@@ -1,6 +1,6 @@
-start powershell -noexit -command "npm run watch-server"
-start powershell -noexit -command "npm run watch-client"
-start powershell -noexit -command "npm run watch-experiment"
-start powershell -noexit
+start powershell -noexit -command "[console]::WindowHeight=20; npm run watch-server"
+start powershell -noexit -command "[console]::WindowHeight=20; npm run watch-client"
+start powershell -noexit -command "[console]::WindowHeight=20; npm run watch-experiment"
+start powershell -noexit -command "[console]::WindowHeight=40"
 
 
