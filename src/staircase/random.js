02532b0a2fe284ed7d59d87c1b61a4fab66a361a
Florian Ledermann <florian.ledermann@tuwien.ac.at>
2021-03-01 10:24:23 +0100

    add random.range generator

--- src/staircase/random.js
+++ src/staircase/random.js
@@ -1,7 +1,19 @@
-module.exports = function*(choices, options) {
+function* randomPick(choices, options) {
   
   while (true) {
     yield choices[Math.floor(choices.length * Math.random())];
   }
   
 }
+
+function* randomRange(from, to, options) {
+  while (true) {
+    yield from + Math.random() * (to-from);
+  }
+
+}
+
+randomPick.pick = randomPick;
+randomPick.range = randomRange;
+
+module.exports = randomPick;
