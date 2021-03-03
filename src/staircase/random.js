255eb1032d3b9409b926fb0d8bf97cbaf247e266
Florian Ledermann <florian.ledermann@tuwien.ac.at>
2021-03-03 17:45:44 +0100

    add random.shuffle generator factory

--- src/staircase/random.js
+++ src/staircase/random.js
@@ -14,9 +14,41 @@ function randomRange(from, to, options) {
   }
 }
 
+function randomShuffle(choices, options) {
+  options = Object.assign({
+    loop: true
+  }, options);
+  
+  function shuffled(arr) {
+    const result = arr.slice();
+    for (let i = result.length - 1; i > 0; i--) {
+      const rand = Math.floor(Math.random() * (i + 1));
+      [result[i], result[rand]] = [result[rand], result[i]];
+    }
+    return result;
+  }
+  
+  shuffledChoices = shuffled(choices);
+  let index = 0;
+  
+  return function*() {
+    
+    while (true) {
+      
+      yield shuffledChoices[index];
+      index++;
+      
+      if (index == shuffledChoices.length) {
+        if (!options.loop) return;
+        shuffledChoices = shuffled(choices);
+        index = 0;
+      }
+    }
+  }
 }
 
 randomPick.pick = randomPick;
 randomPick.range = randomRange;
+randomPick.shuffle = randomShuffle;
 
 module.exports = randomPick;
