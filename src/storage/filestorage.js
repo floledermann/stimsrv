793c9eebe4e679e94363be59a82e7dfd0d74ff58
Florian Ledermann <florian.ledermann@tuwien.ac.at>
2021-03-06 19:32:18 +0100

    delete orphaned lock files, auto-async concurrent calls to getNextParticipantId()

--- src/storage/filestorage.js
+++ src/storage/filestorage.js
@@ -6,9 +6,15 @@ function filestorage(options) {
   options = Object.assign({
     participantId: "user_###",
     filename: "#_YYYY-MM-DD_HH-mm", // https://momentjs.com/docs/#/displaying/format/
+    lockfileName: "#.lock",
     destination: "./data",
     format: "json"    
   }, options);
+  
+  let initTime = new Date();
+  
+  // lock variable to prevent multiple 
+  let inFileOperation = false;
   
   function padNum(num, pad) {
     let str = "" + num;
@@ -16,9 +22,29 @@ function filestorage(options) {
     return padStr + str;
   }
   
+  let userIdStr = (function() {
+    let idLength = options.participantId.match(/#+/)[0].length;
+    return function(numId) {
+      let idStr = padNum(numId, idLength);
+      return options.participantId.replace(/#+/, idStr);
+    }
+  })();
+  
+  // helper function to prevent concurrent calls to a function
+  function preventConcurrent(fn) {
+    
+    let inProgress = Promise.resolve();
+
+    return async function(...args) {
+      await inProgress;
+      inProgress = inProgress.then(() => fn(...args));
+      return inProgress;
+    }
+  }
+  
   async function getNextParticipantId() {
     
-    // if destination dir does not exist, warn and return 1
+    // if destination dir does not exist, warn and return id 1
     try {
       await fs.access(options.destination);
     }
@@ -34,7 +60,6 @@ function filestorage(options) {
 
     // replace ### placeholders with capturing group
     let userIdRegexStr = options.participantId.replace(/#+/,match => "(?<num>\\d{" + match.length + "})");
-    console.log(userIdRegexStr);
     let userIdRegex = new RegExp(userIdRegexStr);
     
     let maxId = 0;
@@ -44,7 +69,23 @@ function filestorage(options) {
     for (let filename of filenames) {
       let num = filename.match(userIdRegex)?.groups?.num;
       if (num) {
+        // convert to number
         num = +num;
+        
+        // check if this is orphaned lockfile - if so, delete
+        let idStr = userIdStr(num);
+        let lockFileName = options.lockfileName.replace("#",idStr);
+        if (filename == lockFileName) {
+          let filePath = path.join(options.destination, filename);
+          let stat = await fs.stat(filePath);
+          if (stat.mtime < initTime) {
+            console.log("Found orphaned lock file: " + filename + " ... deleting.");
+            await fs.unlink(filePath);
+            // do not consider this id for the maximum
+            num = -1;
+          }
+        }
+        
         if (num > maxId) {
           maxId = num;
         }
@@ -52,15 +93,13 @@ function filestorage(options) {
     }
     
     let userId = maxId + 1;
-    
-    let idLength = options.participantId.match(/#+/)[0].length;
-    let idStr = padNum(userId, idLength);
-    
-    idStr = options.participantId.replace(/#+/, idStr);
-    let filename = idStr + ".lock";
-    let filepath = path.join(options.destination, filename);
+       
+    let idStr = userIdStr(userId);
     
     // create empty lock file
+    let filename = options.lockfileName.replace("#",idStr);
+    let filepath = path.join(options.destination, filename);
+    
     try {
       let fileHandle = await fs.open(filepath, "wx");
       await fileHandle?.close();
@@ -76,7 +115,7 @@ function filestorage(options) {
   
   // public API
   return {
-    getNextParticipantId: getNextParticipantId
+    getNextParticipantId: preventConcurrent(getNextParticipantId)
   }
   
   
