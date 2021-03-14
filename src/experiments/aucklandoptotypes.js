dfd2b4eee48d4ade10222cec2642d45652f06d75
Florian Ledermann <florian.ledermann@tuwien.ac.at>
2021-03-14 12:35:44 +0100

    implement vanishing optotypes, selection of subset of shapes

--- src/experiments/aucklandoptotypes.js
+++ src/experiments/aucklandoptotypes.js
@@ -7,23 +7,38 @@ const canvasRenderer = require("./canvasRenderer.js");
 const tao = require("./renderAucklandOptotypes.js");
 
 function renderTAO(ctx, condition) {
-  tao.fill(ctx, condition.shape, condition.size);
+  if (condition.vanishing) {
+    // fill with medium intensity color
+    ctx.fillStyle = canvasRenderer.getColorValueForIntensity((condition.lowIntensity + condition.highIntensity) / 2, condition);
+    ctx.fillRect(-ctx.canvas.width/2-1,-ctx.canvas.height/2-1,ctx.canvas.width+2,ctx.canvas.height+2);
+    if (condition.vanishing == "BWB") {
+      tao.strokeVanishing(condition.backgroundColor, condition.foregroundColor)(ctx, condition.shape, condition.size);
+    }
+    else {
+      tao.strokeVanishing(condition.foregroundColor, condition.backgroundColor)(ctx, condition.shape, condition.size);
+    }
+  }
+  else {
+    tao.fill(ctx, condition.shape, condition.size);
+  }
 }
 
-module.exports = function(parameters, options) {
+module.exports = function(parameters) {
   
   parameters = Object.assign({
-    shape: random.pick(tao.shapeNames),
+    shape: random.pick(parameters?.shapes || tao.shapeNames),
+    shapes: tao.shapeNames,
     size: "10mm",
+    vanishing: false
   }, parameters);
 
-  options = Object.assign({
+  let options = {
     dimensions: ["size"]
-  }, options);
+  };
     
   let renderer = canvasRenderer(renderTAO, options);
   
-  let buttonCanvas = htmlButtons.buttonCanvas(renderTAO, {size: "27arcmin"}, options);
+  let buttonCanvas = htmlButtons.buttonCanvas(renderTAO, {size: "27arcmin", vanishing: false}, options);
 
   function firstCap(str) {
     return str.charAt(0).toUpperCase() + str.slice(1);
@@ -34,7 +49,7 @@ module.exports = function(parameters, options) {
     description: "Auckland Optotypes visual acuity test", 
     interfaces: {
       display: renderer,
-      response: htmlButtons(tao.shapeNames.map(name => ({
+      response: htmlButtons(parameters.shapes.map(name => ({
         label: firstCap(name),
         canvas: buttonCanvas,
         response: {shape: name}
