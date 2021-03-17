2f6c2e4adb20d9d2914fcb8f6dcc119b5c5ef3db
Florian Ledermann <florian.ledermann@tuwien.ac.at>
2021-03-17 21:15:55 +0100

    new client build

--- static/client-browser.js
+++ static/client-browser.js
@@ -7080,7 +7080,7 @@ function clientFactory(options) {
   
   function prepareExperiment(experiment) {
     
-    for (ui of options.role.interfaces) {
+    for (let ui of options.role.interfaces) {
       
       // clear ui
       let wrapper = document.getElementById("interface-" + ui);
@@ -7094,7 +7094,7 @@ function clientFactory(options) {
   }
   
   function showCondition(experiment, condition) {
-    for (ui of options.role.interfaces) {
+    for (let ui of options.role.interfaces) {
       if (experiment.interfaces[ui]) {
         experiment.interfaces[ui]?.render?.(condition);
       }
