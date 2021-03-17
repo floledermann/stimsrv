4b6c1c5792150c86618e02268a13aad28365cadf
Florian Ledermann <florian.ledermann@tuwien.ac.at>
2021-03-17 21:15:44 +0100

    remove unneeded build scripts - experiment is now bundled on the fly by server

--- _dev.bat
+++ _dev.bat
@@ -1,6 +1,5 @@
 start powershell -noexit -command "[console]::WindowHeight=20; npm run watch-server"
 start powershell -noexit -command "[console]::WindowHeight=20; npm run watch-client"
-start powershell -noexit -command "[console]::WindowHeight=20; npm run watch-experiment"
 start powershell -noexit -command "[console]::WindowHeight=40"
 
 
