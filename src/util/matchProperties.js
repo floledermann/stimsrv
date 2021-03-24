f7f1d64eb2b94aa97726c0c550c8113685306baf
Florian Ledermann <florian.ledermann@tuwien.ac.at>
2021-03-24 19:21:07 +0100

    recursively match response properties

--- src/util/matchProperties.js
+++ src/util/matchProperties.js
@@ -1,6 +1,15 @@
 // Return true iff each entry in template has a matching entry in obj
 function matchProperties(obj, template) {
-  return Object.entries(template).every(([key, value]) => obj[key] == value);
+  return Object.entries(template).every(([key, value]) => {
+    // recurs arrays and objects
+    if (Array.isArray(obj[key])) {
+      return Array.isArray(value) && matchProperties(obj[key], value);
+    }
+    if (typeof obj[key] == "object") {
+      return (typeof value == "object") && matchProperties(obj[key], value);
+    }
+    return obj[key] == value
+  });
 }
 
 matchProperties.strict = function(obj, template) {
