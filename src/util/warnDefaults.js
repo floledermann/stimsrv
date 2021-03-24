61e8a96dc2679074e4ba4f749362964e0a3266b7
Florian Ledermann <florian.ledermann@tuwien.ac.at>
2021-03-24 15:38:30 +0100

    emit a warning when default values are accessed

--- src/util/warnDefaults.js
+++ src/util/warnDefaults.js
@@ -1,9 +1,14 @@
 function warnDefaults(warningFunction, object, defaults) {
   
   for (let key of Object.keys(defaults)) {
-    if (! key in object) {
-      warningFunction("No value for " + key + " provided, using default of " + defaults[key] + "!");
-      object[key] = defaults[key];
+    if (! (key in object)) {
+      //warningFunction("No value for " + key + " provided, using default of " + defaults[key] + "!");
+      //object[key] = defaults[key];
+      let val = defaults[key];
+      Object.defineProperty(object, key, { get: function() {
+        warningFunction("No value for " + key + " provided, using default of " + defaults[key] + ".");
+        return val;
+      } });
     }
   }
   
