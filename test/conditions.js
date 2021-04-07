7748c11ded2968efd55d53f2a58ee4cec2f43d78
Florian Ledermann <florian.ledermann@tuwien.ac.at>
2021-04-07 16:47:04 +0200

    add dummy test file for conditions

---
+++ test/conditions.js
@@ -0,0 +1,9 @@
+"use strict";
+
+const assert = require("assert");
+
+const sequence = require("../src/controller/sequence.js");
+
+const {controllerTask, controllerTasks, controllersExperiment} = require("./_util.js");
+
+
