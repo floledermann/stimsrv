a51804ddbd1a1429109e7cea92c2cd35f0ae655d
Florian Ledermann <florian.ledermann@tuwien.ac.at>
2021-04-11 13:04:16 +0200

    pick object properties utility

---
+++ src/util/pickProperties.js
@@ -0,0 +1,22 @@
+
+function pickProperties(obj, keys) {
+  let result = {};
+  for (let key of keys) {
+    if (obj.hasOwnProperty(key) {
+      result[key] = obj[key];
+    }
+  }
+  return result;
+}
+
+pickProperties.without(obj, omitKeys) {
+  let result = {};
+  for (let key of Object.keys(obj)) {
+    if (!omitKeys.includes(key) {
+      result[key] = obj[key];
+    }
+  }
+  return result;
+}
+
+module.exports = pickProperties;
