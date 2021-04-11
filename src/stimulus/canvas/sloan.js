dcad44260f2c51e50814ce12900861b95c9c862b
Florian Ledermann <florian.ledermann@tuwien.ac.at>
2021-04-11 11:52:56 +0200

    clean up coordinates

--- src/stimulus/canvas/sloan.js
+++ src/stimulus/canvas/sloan.js
@@ -1,83 +1,81 @@
 // Sloan Letters
 // Based on https://en.wikipedia.org/wiki/File:ETDRS_Chart_R.svg (Public Domain)
 
-// Converted from SVG with Canvg / http://www.professorcloud.com/svg-to-canvas/
-
 const letters = ["C", "D", "H", "K", "N", "O", "R", "S", "V", "Z"];
 
 const paths = {
   C: ctx => {
-    ctx.moveTo(9.8974,5.9988);
-    ctx.lineTo(7.818999999999999,5.9988);
-    ctx.bezierCurveTo(7.39764,7.1578,6.3005,8.00100,4.9995,8.00100);
-    ctx.bezierCurveTo(3.361,8.00100,1.9983,6.63830,1.9983,4.99960);
-    ctx.bezierCurveTo(1.9983,3.36090,3.361,1.99840,4.9995,1.99840);
-    ctx.bezierCurveTo(6.3003,1.99840,7.3974,2.84136,7.819,4.00060);
-    ctx.lineTo(9.8974,4.00060);
-    ctx.bezierCurveTo(9.43953,1.718505,7.4195,0,4.9996,0);
-    ctx.bezierCurveTo(2.238199999999999,0,0,2.23840,0,4.99960);
-    ctx.bezierCurveTo(0,7.760805,2.2382,9.99920,4.9996,9.99920);
-    ctx.bezierCurveTo(7.4194,9.99920,9.4397,8.28070,9.8974,5.99880);
+    ctx.moveTo(9.8974,6);
+    ctx.lineTo(7.819,6);
+    ctx.bezierCurveTo(7.39764,7.1578,6.3005,8,5,8);
+    ctx.bezierCurveTo(3.361,8.00100,1.9983,6.63830,1.9983,5);
+    ctx.bezierCurveTo(1.9983,3.36090,3.361,1.99840,4.9995,2);
+    ctx.bezierCurveTo(6.3003,1.99840,7.3974,2.84136,7.819,4);
+    ctx.lineTo(9.8974,4);
+    ctx.bezierCurveTo(9.43953,1.718505,7.4195,0,5,0);
+    ctx.bezierCurveTo(2.2382,0,0,2.23840,0,5);
+    ctx.bezierCurveTo(0,7.760805,2.2382,10,5,10);
+    ctx.bezierCurveTo(7.4194,9.99920,9.4397,8.28070,9.8974,6);
     ctx.closePath();
   },
   D: ctx => {
-    ctx.moveTo(9.9992,5.9996);
-    ctx.lineTo(9.9992,4.0012);
-    ctx.bezierCurveTo(9.9992,1.7993,8.2007,0.0008,5.9988,0.0008);
-    ctx.lineTo(0,0.0008);
+    ctx.moveTo(10,6);
+    ctx.lineTo(10,4);
+    ctx.bezierCurveTo(10,1.8,8.2,0,6,0);
+    ctx.lineTo(0,0);
     ctx.lineTo(0,10);
-    ctx.lineTo(5.9988,10);
-    ctx.bezierCurveTo(8.20070,10,9.9992,8.2015,9.9992,5.9996);
+    ctx.lineTo(6,10);
+    ctx.bezierCurveTo(8.2,10,10,8.2,10,6);
     ctx.closePath();
-    ctx.moveTo(8.0008,5.9996);
-    ctx.bezierCurveTo(8.0008,7.1004,7.09971,8.0018,5.9986,8.0018);
-    ctx.lineTo(1.9987,8.0018);
-    ctx.lineTo(1.9987,1.999299999999999);
-    ctx.lineTo(5.9991,1.999299999999999);
-    ctx.bezierCurveTo(7.0999,1.9993,8.0013,2.90039,8.0013,4.0015);
+    ctx.moveTo(8,6);
+    ctx.bezierCurveTo(8,7.1,7.1,8,6,8);
+    ctx.lineTo(2,8);
+    ctx.lineTo(2,2);
+    ctx.lineTo(6,2);
+    ctx.bezierCurveTo(7.1,2,8,2.9,8,4);
     ctx.closePath();
   },
   H: ctx => {
     ctx.moveTo(10,10);
     ctx.lineTo(10,0);
-    ctx.lineTo(8.0006,0);
-    ctx.lineTo(8.0006,3.9989);
-    ctx.lineTo(1.999500,3.9989);
-    ctx.lineTo(1.999500,0);
-    ctx.lineTo(0.0001001103,0);
-    ctx.lineTo(0.0001001103,10);
-    ctx.lineTo(1.999500,10);
-    ctx.lineTo(1.999500,6.0011);
-    ctx.lineTo(8.0006,6.0011);
-    ctx.lineTo(8.0006,10);
+    ctx.lineTo(8,0);
+    ctx.lineTo(8,4);
+    ctx.lineTo(2,4);
+    ctx.lineTo(2,0);
+    ctx.lineTo(0,0);
+    ctx.lineTo(0,10);
+    ctx.lineTo(2,10);
+    ctx.lineTo(2,6);
+    ctx.lineTo(8,6);
+    ctx.lineTo(8,10);
     ctx.closePath();
   },
   K: ctx => {
     ctx.moveTo(5.7409,3.2201);
-    ctx.lineTo(9.9992,0);
+    ctx.lineTo(10,0);
     ctx.lineTo(6.6783,0);
-    ctx.lineTo(1.998400,3.5398);
-    ctx.lineTo(1.998400,0);
+    ctx.lineTo(2,3.5398);
+    ctx.lineTo(2,0);
     ctx.lineTo(0,0);
     ctx.lineTo(0,10);
-    ctx.lineTo(1.998400,10);
-    ctx.lineTo(1.998400,6.0396);
+    ctx.lineTo(2,10);
+    ctx.lineTo(2,6.0396);
     ctx.lineTo(4.01870,4.5209);
     ctx.lineTo(7.45960,10);
-    ctx.lineTo(9.99920,10);
+    ctx.lineTo(10,10);
     ctx.closePath();
   },
   N: ctx => {
     ctx.moveTo(10,10);
     ctx.lineTo(10,0);
-    ctx.lineTo(8.0014,0);
-    ctx.lineTo(8.0014,6.9186);
-    ctx.lineTo(1.9986000000000006,0);
+    ctx.lineTo(8,0);
+    ctx.lineTo(8,6.9186);
+    ctx.lineTo(2,0);
     ctx.lineTo(0,0);
     ctx.lineTo(0,10);
-    ctx.lineTo(1.9986,10);
-    ctx.lineTo(1.9986,3.081400);
-    ctx.lineTo(8.0014,10);
+    ctx.lineTo(2,10);
+    ctx.lineTo(2,3.081400);
+    ctx.lineTo(8,10);
     ctx.closePath();
   },
   O: ctx => {
@@ -90,7 +88,7 @@ const paths = {
     ctx.moveTo(7.9978,5);
     ctx.bezierCurveTo(7.9978,6.6378,6.6378,7.9978,5,7.9978);
     ctx.bezierCurveTo(3.3622,7.9978,2.0022,6.6378,2.0022,5);
-    ctx.bezierCurveTo(2.0022,3.3622000000000005,3.3622,2.0022,5,2.0022);
+    ctx.bezierCurveTo(2.0022,3.3622,3.3622,2.0022,5,2.0022);
     ctx.bezierCurveTo(6.6378,2.0022,7.9978,3.3622,7.9978,5);
     ctx.closePath();
   },
@@ -98,21 +96,17 @@ const paths = {
     ctx.moveTo(10,10);
     ctx.lineTo(8.183,5.7581);
     ctx.translate(7.017036266176826,3.01240927243997);
-    ctx.rotate(0);
     ctx.arc(0,0,2.983,1.1692202671320828,-0.004931053318863343,1);
-    ctx.rotate(0);
     ctx.translate(-7.017036266176826,-3.01240927243997);
     ctx.translate(7.0019000150080055,2.997999984991995);
-    ctx.rotate(0);
     ctx.arc(0,0,2.9981,-0.0001,-1.5706962684264127,1);
-    ctx.rotate(0);
     ctx.translate(-7.0019,-2.998);
-    ctx.lineTo(0,-0.0001);
-    ctx.lineTo(0,9.9999);
-    ctx.lineTo(2.0022,9.9999);
-    ctx.lineTo(2.0022,6.001);
-    ctx.lineTo(6.1226,6.001);
-    ctx.lineTo(7.8185,9.9999);
+    ctx.lineTo(0,0);
+    ctx.lineTo(0,10);
+    ctx.lineTo(2,10);
+    ctx.lineTo(2,6);
+    ctx.lineTo(6.1226,6);
+    ctx.lineTo(7.8185,10);
     ctx.closePath();
     ctx.moveTo(7.9978,2.9978);
     ctx.bezierCurveTo(7.9978,3.54187,7.5578,3.9989,7.00224,3.9989);
@@ -123,11 +117,11 @@ const paths = {
     ctx.closePath();
   },
   S: ctx => {
-    ctx.moveTo(10,3.0006);
+    ctx.moveTo(10,3);
     ctx.bezierCurveTo(10,2.19912,9.60074,1.441,9.18111,0.9606);
     ctx.lineTo(9.16074,0.9606);
-    ctx.bezierCurveTo(8.4,0.10134,7.48004,0.00004,6.99964,0.00004);
-    ctx.lineTo(3.000543,0.00004);
+    ctx.bezierCurveTo(8.4,0.10134,7.48004,0,6.99964,0);
+    ctx.lineTo(3,0);
     ctx.bezierCurveTo(1.979042,0.00004,1.339443,0.5006,0.960543,0.8593);
     ctx.bezierCurveTo(-0.26056,1.9993,-0.35886,3.9612,0.960543,5.1593);
     ctx.bezierCurveTo(1.379983,5.54115,2.080344,6.00115,3.000543,6.00115);
@@ -138,10 +132,10 @@ const paths = {
     ctx.lineTo(3.000548,8.00053);
     ctx.bezierCurveTo(2.60128,8.00053,2.45943,7.939797,2.25980,7.74017);
     ctx.bezierCurveTo(2.06017,7.54055,1.99943,7.30036,1.99943,6.99943);
-    ctx.lineTo(0.00003972893,6.99943);
+    ctx.lineTo(0,6.99943);
     ctx.bezierCurveTo(0.00003972893,7.69943,0.2400396,8.55903,0.89984,9.18093);
     ctx.bezierCurveTo(1.519101,9.76000,2.23944,10.00000,3.000548,10.00000);
-    ctx.lineTo(6.99944,10.00000);
+    ctx.lineTo(6.99944,10);
     ctx.bezierCurveTo(8.07874,10.00000,8.57924,9.67868,9.18114,8.99870);
     ctx.bezierCurveTo(10.28054,7.780605,10.28924,5.90830,9.03929,4.820606);
     ctx.bezierCurveTo(8.68929,4.511155,8.02079,3.998934,6.99929,3.998934);
@@ -157,7 +151,7 @@ const paths = {
   V: ctx => {
     ctx.moveTo(10,0);
     ctx.lineTo(7.8385,0);
-    ctx.lineTo(5,7.12090037793);
+    ctx.lineTo(5,7.1209);
     ctx.lineTo(2.1585,0);
     ctx.lineTo(0,0);
     ctx.lineTo(3.9989,10.0);
@@ -166,14 +160,14 @@ const paths = {
   },
   Z: ctx => {
     ctx.moveTo(10,10);
-    ctx.lineTo(10,8.0006);
-    ctx.lineTo(3.0787,8.0006);
-    ctx.lineTo(10,1.9995);
-    ctx.lineTo(10,0.0001);
-    ctx.lineTo(0,0.0001);
-    ctx.lineTo(0,1.9995);
-    ctx.lineTo(6.9213,1.9995);
-    ctx.lineTo(0,8.0006);
+    ctx.lineTo(10,8);
+    ctx.lineTo(3.0787,8);
+    ctx.lineTo(10,2);
+    ctx.lineTo(10,0);
+    ctx.lineTo(0,0);
+    ctx.lineTo(0,2);
+    ctx.lineTo(6.9213,2);
+    ctx.lineTo(0,8);
     ctx.lineTo(0,10);
     ctx.closePath();
   }
