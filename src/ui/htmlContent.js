774946e411645b16e40216af5528384abc4838bb
Florian Ledermann <florian.ledermann@tuwien.ac.at>
2021-04-12 18:35:14 +0200

    allow setting style

--- src/ui/htmlContent.js
+++ src/ui/htmlContent.js
@@ -3,24 +3,31 @@ module.exports = function(htmlString, options) {
   
   options = Object.assign({
     wrapperTag: "div",
-    wrapperClass: "content"
-  });
+    wrapperClass: "content",
+    style: null
+  }, options);
   
   let parent = null;
+  let document = null;
   
   return {
-    initialize: function(_parent, _runtime) {
+    initialize: function(_parent) {
       parent = _parent;
+      document = parent.ownerDocument;
     },
     
     // contract: if render() returns a string or element, then replace the parent content
     render: function(condition) {
-      let start="", end="";
-      if (options.wrapperTag) {
-        start = "<" + options.wrapperTag + (options.wrapperClass ? ' class="' + options.wrapperClass + '"' : "") + ">";
-        end = "</" + options.wrapperTag + ">";
-      }
-      parent.innerHTML = start + htmlString + end;
+      
+      let wrapper = document.createElement(options.wrapperTag || "div");
+      
+      if (options.wrapperClass) wrapper.className = options.wrapperClass;
+      if (options.style) parent.style.cssText = options.style;
+      
+      wrapper.innerHTML = htmlString;
+      
+      parent.innerHTML = "";
+      parent.appendChild(wrapper);
     }
   }
 }
