78c6ad565ba800f8eadb2167903a8e258429ede1
Florian Ledermann <florian.ledermann@tuwien.ac.at>
2021-04-13 08:25:43 +0200

    target ui selection

--- src/task/snellen.js
+++ src/task/snellen.js
@@ -6,39 +6,59 @@ const random = require("../controller/random.js");
 const canvasRenderer = require("../stimulus/canvas/canvasRenderer.js");
 const renderSnellen = require("../stimulus/canvas/snellen.js");
 
-module.exports = function(parameters, options) {
+const valOrFunc = require("../util/valOrFunc.js");
+const pick = require("../util/pickProperties.js");
+
+module.exports = function(config) {
   
-  parameters = Object.assign({
+  config = Object.assign({
     angle: random.pick([0,90,180,270]),
     size: "10mm",
     middleBar: true,
-    pixelAlign: true
-  }, parameters);
-
-  options = Object.assign({
+    pixelAlign: true,
+    stimulusDisplay: "display",
+    responseDisplay: "response",
+    monitorDisplay: "monitor",
     dimensions: ["size"]
-  }, options);
+  }, config);
+
+  let parameters = pick.without(config, ["stimulusDisplay","responseDisplay","monitorDisplay","dimensions"]);
     
-  let renderer = canvasRenderer(renderSnellen, options);
+  let renderer = canvasRenderer(renderSnellen, config);
   
-  let buttonCanvas = htmlButtons.buttonCanvas(renderSnellen, {size: "27arcmin"}, options);
+  let buttonCanvas = htmlButtons.buttonCanvas(renderSnellen, {size: "27arcmin"}, config);
 
   return {
+    
     name: "snellen",
+    
     description: "Snellen-E visual acuity test", 
+    
     ui: function(context) {
+      
+      let interfaces = {};
+      
+      let responseButtons = htmlButtons([
+        {label: "Left", canvas: buttonCanvas, response: {angle: 180}},
+        {label: "Up", canvas: buttonCanvas, response: {angle: 270}},
+        {label: "Down", canvas: buttonCanvas, response: {angle: 90}},
+        {label: "Right", canvas: buttonCanvas, response: {angle: 0}}
+      ]);
+      
+      for (let ui of valOrFunc.array(config.stimulusDisplay, context)) {
+        interfaces[ui] = renderer;
+      }
+      
+      for (let ui of valOrFunc.array(config.responseDisplay, context)) {
+        interfaces[ui] = responseButtons;
+      }
+      
+      for (let ui of valOrFunc.array(config.monitorDisplay, context)) {
+        interfaces[ui] = renderer;
+      }
+      
       return {
-        interfaces: {
-          display: renderer,
-          response: htmlButtons([
-            {label: "Left", canvas: buttonCanvas, response: {angle: 180}},
-            {label: "Up", canvas: buttonCanvas, response: {angle: 270}},
-            {label: "Down", canvas: buttonCanvas, response: {angle: 90}},
-            {label: "Right", canvas: buttonCanvas, response: {angle: 0}}
-          ]),
-          monitor: renderer,
-          control: null
-        }
+        interfaces: interfaces
       }
     },
     controller: parameterController({parameters: parameters})
