2347832493442559cfc345a6f3f47b87a501ef94
Florian Ledermann <florian.ledermann@tuwien.ac.at>
2021-07-09 15:50:43 +0200

    rename config -> spec

--- src/task/simpleTask.js
+++ src/task/simpleTask.js
@@ -2,8 +2,9 @@
 const taskManager = require("stimsrv/task/taskManager");
 const toArray = require("stimsrv/util/toArray");
 
-function simpleTask(config) {
-  config = Object.assign({
+function simpleTask(taskSpec) {
+  
+  taskSpec = Object.assign({
     name: "Unnamed Task",
     description: "",
     defaults: {},
@@ -12,35 +13,37 @@ function simpleTask(config) {
     nextContext: null,
     //resources: null, //?
     //css: null //?
-  }, config);
+  }, taskSpec);
   
   let task = function(controllerConfig, transformCondition) {
     
-    let interfaceOptions = Object.keys(config.interfaces).map(i => i + "Interface");
+    let interfaceOptions = Object.keys(taskSpec.interfaces).map(i => i + "Interface");
     let staticOptions = interfaceOptions.concat(["css"]);
     
-    //let interfaceConstructors = Object.fromEntries(Object.entries(config.interfaces).map([key, spec] => [key, spec(config)]));
+    //let interfaceConstructors = Object.fromEntries(Object.entries(taskSpec.interfaces).map([key, spec] => [key, spec(taskSpec)]));
     
     let manager = taskManager({
-      defaults: config.defaults,
+      defaults: taskSpec.defaults,
       controllerConfig: controllerConfig,
       transformCondition: transformCondition,
-      nextContext: config.nextContext,
+      nextContext: taskSpec.nextContext,
       // do we need this? may simply throw an error if it does not resolve to a static value
       staticOptions: staticOptions
     });
+    
+    let config = manager.resolveConfig();
     
     return {
       name: config.name,
       description: config.description,
       frontend: context => {
         return {
-          interfaces: manager.interfaces(config.interfaces, context),
+          interfaces: manager.interfaces(taskSpec.interfaces, context),
           transformCondition: manager.transformCondition(context)
         };
       },
       controller: manager.controller,
-      resources: toArray(config.resources).map(res => {
+      resources: toArray(taskSpec.resources).map(res => {
         if (typeof res == "string" || typeof res == "function") {
           return manager.resolveResources(res);
         }
@@ -50,7 +53,7 @@ function simpleTask(config) {
   }
   
   task.defaults = function(_defaults) {
-    config.defaults = Object.assign({}, config.defaults, _defaults);
+    taskSpec.defaults = Object.assign({}, taskSpec.defaults, _defaults);
   }
   
   return task;
