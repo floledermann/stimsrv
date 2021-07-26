d7036223009cefc42a2f68a36d7ce0511ae5d483
Florian Ledermann <florian.ledermann@tuwien.ac.at>
2021-07-26 11:35:43 +0200

    render only when resources have been loaded

--- src/stimulus/canvas/canvasRenderer.js
+++ src/stimulus/canvas/canvasRenderer.js
@@ -35,7 +35,7 @@ function canvasRenderer(renderFunc, options) {
     let dppx = 1;
     let width = 0, height = 0;
     
-    let resourcesPromise = null;
+    let resourcesPromise = Promise.resolve(true);
     
     return {
       initialize: function(parentOrCanvas, _runtime) {
@@ -121,94 +121,92 @@ function canvasRenderer(renderFunc, options) {
         // remember for redrawing on resize
         lastCondition = condition;
         
-        let width = ctx2d.canvas.width;
-        let height = ctx2d.canvas.height;
-          
-        condition = Object.assign({
-          lowIntensity: 0,
-          // highIntensity: 1.0,
-          // contrastRatio: 2.0,           // maximum contrast based on minimumIntensityColor, maximumIntensityColor
-          foregroundIntensity: 1.0,  // high intensity (bright) stimulus on low intensity background.
-          backgroundIntensity: 0.0,
-          rotate: 0
-        }, condition);
-        
-        // convert dimensions to pixels
-        for (let key of options.dimensions) {
-          let cond = condition[key];
-          if (Array.isArray(cond)) {
-            condition[key] = cond.map(c => display.dimensionToScreenPixels(c, condition));
-          }
-          else {
-            condition[key] = display.dimensionToScreenPixels(cond, condition)  
-            //console.log("Converting " + cond + " to " + condition[key]);
-          }
-        }
-        
-       // default case, use maximum intensity as high intensity
-        if ((!condition.highIntensity) && (!condition.contrastRatio)) {
-          condition.highIntensity = 1.0;
-        }
-        
-        if (condition.contrastRatio) {
-          if (condition.highIntensity) {
-            runtime.warn("Both highIntensity and contrastRatio specified - omitting contrastRatio.");
-          }
-          else {
-            let realLowIntensity = condition.lowIntensity + runtime.ambientIntensity * (1-condition.lowIntensity);
-            let realHighIntensity = realLowIntensity * condition.contrastRatio;
-            condition.highIntensity = (realHighIntensity - runtime.ambientIntensity) / (1 - runtime.ambientIntensity);
-          }
-        }
-        /*
-        if (condition.contrastRatio / 2 > condition.centerIntensity) {
-          runtime.warn("Contrast ratio " + condition.contrastRatio + " exceeds valid range for center intensity " + condition.centerIntensity + " - clipping will occur!");
-        }
-        */
-
-        // convert intensities to color values
-        for (let key of options.intensities) {
-          condition[key] = display.intensityToColorValue(condition[key], condition);
-        }
-        
-        ctx2d.resetTransform();
-        
-        if (condition.backgroundIntensity) {
-          ctx2d.fillStyle = condition.backgroundIntensity;
-          ctx2d.fillRect(0,0,width,height);
-          if (!this.backgroundColor) this.backgroundColor = condition.backgroundIntensity; // HACK for browser-simple, refactor
-        }
-        
-        if (condition.foregroundIntensity) {
-          ctx2d.fillStyle = condition.foregroundIntensity;
-          ctx2d.strokeStyle = condition.foregroundIntensity;
-          if (!this.foregroundColor) this.foregroundColor = condition.foregroundIntensity; // HACK for browser-simple, refactor
-        }
-        
-        // move origin to center
-        ctx2d.translate(Math.round(width / 2), Math.round(height / 2));
-        
-        // affine transform
-        if (condition.translate) {
-          let trans = condition.translate;
-          if (!Array.isArray(condition.translate)) {
-            trans = [trans, trans];
-          }
-          ctx2d.translate(trans[0], trans[1]);
-        }  
-        if (condition.rotate) {
-          ctx2d.rotate(condition.rotate/180*Math.PI);
-        }
-        
         // wait for resources to be loaded
-        if (resourcesPromise) {
-          resourcesPromise.then(() => {
-            renderFunc(ctx2d, condition);
-          });
-        }
-        else {
+        resourcesPromise.then(() => {
+          
+          let width = ctx2d.canvas.width;
+          let height = ctx2d.canvas.height;
+            
+          condition = Object.assign({
+            lowIntensity: 0,
+            // highIntensity: 1.0,
+            // contrastRatio: 2.0,           // maximum contrast based on minimumIntensityColor, maximumIntensityColor
+            foregroundIntensity: 1.0,  // high intensity (bright) stimulus on low intensity background.
+            backgroundIntensity: 0.0,
+            rotate: 0
+          }, condition);
+          
+          // convert dimensions to pixels
+          for (let key of options.dimensions) {
+            let cond = condition[key];
+            if (Array.isArray(cond)) {
+              condition[key] = cond.map(c => display.dimensionToScreenPixels(c, condition));
+            }
+            else {
+              condition[key] = display.dimensionToScreenPixels(cond, condition)  
+              //console.log("Converting " + cond + " to " + condition[key]);
+            }
+          }
+          
+         // default case, use maximum intensity as high intensity
+          if ((!condition.highIntensity) && (!condition.contrastRatio)) {
+            condition.highIntensity = 1.0;
+          }
+          
+          if (condition.contrastRatio) {
+            if (condition.highIntensity) {
+              runtime.warn("Both highIntensity and contrastRatio specified - omitting contrastRatio.");
+            }
+            else {
+              let realLowIntensity = condition.lowIntensity + runtime.ambientIntensity * (1-condition.lowIntensity);
+              let realHighIntensity = realLowIntensity * condition.contrastRatio;
+              condition.highIntensity = (realHighIntensity - runtime.ambientIntensity) / (1 - runtime.ambientIntensity);
+            }
+          }
+          /*
+          if (condition.contrastRatio / 2 > condition.centerIntensity) {
+            runtime.warn("Contrast ratio " + condition.contrastRatio + " exceeds valid range for center intensity " + condition.centerIntensity + " - clipping will occur!");
+          }
+          */
+
+          // convert intensities to color values
+          for (let key of options.intensities) {
+            condition[key] = display.intensityToColorValue(condition[key], condition);
+          }
+          
+          ctx2d.resetTransform();
+          
+          if (condition.backgroundIntensity) {
+            ctx2d.fillStyle = condition.backgroundIntensity;
+            ctx2d.fillRect(0,0,width,height);
+            if (!this.backgroundColor) this.backgroundColor = condition.backgroundIntensity; // HACK for browser-simple, refactor
+          }
+          
+          if (condition.foregroundIntensity) {
+            ctx2d.fillStyle = condition.foregroundIntensity;
+            ctx2d.strokeStyle = condition.foregroundIntensity;
+            if (!this.foregroundColor) this.foregroundColor = condition.foregroundIntensity; // HACK for browser-simple, refactor
+          }
+          
+          // move origin to center
+          ctx2d.translate(Math.round(width / 2), Math.round(height / 2));
+          
+          // affine transform
+          if (condition.translate) {
+            let trans = condition.translate;
+            if (!Array.isArray(condition.translate)) {
+              trans = [trans, trans];
+            }
+            ctx2d.translate(trans[0], trans[1]);
+          }  
+          if (condition.rotate) {
+            ctx2d.rotate(condition.rotate/180*Math.PI);
+          }
+          
           renderFunc(ctx2d, condition);
-        }
+          
+        });
+
       },
       
       fonts: options.fonts,
